0835008f83ada1106fd1556b530dcf8159541387
(Louis)Hao Lu <hlu47@bloomberg.net>
2016-09-02 15:32:32 -0400

    start 90 degree

--- USACO/1.2/Transformations/transform.m.cpp
+++ USACO/1.2/Transformations/transform.m.cpp
@@ -9,17 +9,100 @@ LANG: C++
 
 using namespace std;
 
-bsl::string startPattern[100];
-bsl::string endPattern[100];
+class Pattern
+{
+public:
+    //should check size < 10 in init
+    Pattern(int size, char **startPattern):d_size(size)
+    {
+        for(int i = 0; i < size; i++)
+        for(int j = 0; j < size; j++)
+        {
+            d_pattern[i][j] = startPattern[i][j];
+        }
+    }
+
+    Pattern(int size):d_size(size){};
+
+    friend ostream& operator<<(ostream& os, const Pattern &p)
+    {
+        int size = p.d_size;
+        for(int i = 0; i < size; i++)
+        {
+                for(int j = 0; j < size; j++)
+                {
+                    os << p.d_pattern[i][j];
+                }
+                os<<'\n';
+            }
+            return os;
+    }
+
+    friend istream& operator>>(istream& is, Pattern &p)
+    {
+        int size = p.d_size;
+        for(int i = 0; i < size; i++)
+        {
+            string line;
+            is >> line;
+            for(int j = 0; j < size; j++)
+            {
+                p.d_pattern[i][j] = line[j];
+            }
+        }
+        return is;
+    }
+
+    bool operator==(const Pattern &p2) const
+    {
+        const Pattern &p1 = *this;
+        if(p1.d_size != p2.d_size) return false;
+        int size = p1.d_size;
+        for(int i = 0; i < size; i++)
+        for(int j = 0; j < size; j++)
+        {
+            if(p1.d_pattern[i][j] != p2.d_pattern[i][j])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    Pattern rotationNinetyDegree()
+    {
+        Pattern after(this->d_size);
+
+    
+    }
+
+    chat * charAt(int i, int j)
+    {
+        //should check i j
+        return d_pattern[i][j];
+    }
 
 
 
+private:
+    char d_pattern[10][10];
+    int  d_size;
+
+};
 
 int main() {
     ofstream fout ("transform.out");
     ifstream fin ("transform.in");
-    int a, b;
-    fin >> a >> b;
-    fout << a+b << endl;
+    int n;
+    fin>>n;
+    Pattern start(n), end(n);
+    fin>>start;
+    fin>>end;
+    cout<<start;
+    cout<<endl;
+    cout<<end;
+    cout<<(start == end)<<endl;
+
+
     return 0;
 }
