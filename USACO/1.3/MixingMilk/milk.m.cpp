fb21274fe923bc5c0dab2227f2c92c6ffe1a904b
(Louis)Hao Lu <hlu47@bloomberg.net>
2016-09-15 14:26:04 -0400

    init mixing milk problem

---
+++ USACO/1.3/MixingMilk/milk.m.cpp
@@ -0,0 +1,55 @@
+/*
+ID: louislu1
+PROG: milk 
+LANG: C++
+*/
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int main() {
+    ofstream fout ("milk.out");
+    ifstream fin ("milk.in");
+    int totalMilk;
+    map<int,int> farms;
+    fin >> totalMilk;
+    int n;
+    fin >> n;
+    for(int i = 0; i < n; i++)
+    {
+        int price, amount;
+        fin>>price>>amount;
+        farms.insert(pair<int, int>(price, amount));
+    }
+
+    int totalCost = 0;
+    //run greedy
+    map<int,int>::iterator iter = farms.begin();
+    //never check end cause The total milk produced per day by 
+    //the farmers will always be sufficient to meet the demands 
+    //of the Merry Milk Makers even if the prices are high.
+    while(totalMilk > 0)
+    {
+        int price, amount;
+        price = iter->first;
+        amount = iter->second;
+
+        if(amount >= totalMilk)
+        {
+            totalCost += price * totalMilk;
+            totalMilk = 0;
+            break;
+        }
+
+        totalCost += price * amount;
+        totalMilk -= amount;
+        iter++;
+    } 
+
+    fout<<totalCost<<endl;
+
+    return 0;
+}
